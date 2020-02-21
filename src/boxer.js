8c2d87af7929e1bad1fdf605ff982070b0ee7a29
Tuan <Tuan.viet.nguyen@hotmail.co.uk>
2020-02-21 22:31:46 +0000

    Chore: install server to use module

--- src/boxer.js
+++ src/boxer.js
@@ -1,2 +1,17 @@
-import {Boxrec as boxrec} from "boxrec";
 
+// try {
+//   const cookieJar = await boxrec.login(TUAN_fish, Password12);
+//   // successfully logged in
+// } catch (e) {
+//   console.log('error ')
+// }
+
+
+// const gennadyGolovkin = await boxrec.getPersonById(cookieJar, 356831);
+// console.log(gennadyGolovkin.name); // Gennady Golovkin
+// console.log(gennadyGolovkin.division); // middleweight
+// console.log(gennadyGolovkin.titlesHeld); // currently held titles
+// console.log(gennadyGolovkin.otherInfo); // other info that couldn't be categorized
+// console.log(gennadyGolovkin.suspended); // will tell if boxer is currently suspended
+// console.log(gennadyGolovkin.bouts); // list of bouts
+// console.log(gennadyGolovkin.bouts[37].opponent.name); // Saul Alvarez
